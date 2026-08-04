599420a1b93a3531d19a3daee236a9002fc54931
Kaveri <kaverijakke9@gmail.com>
2026-08-04 11:23:09 +0530

    Revert "Change greeting message in Hello.java"
    This reverts commit 29ce4d02c092cef802563fbfb75f5f81021a07ef.

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
 public static String greet(String name){
-return "Welcome to devops lab" + " " +name;
+return "HELLO" + " " +name;
 }
 public static void main(String[] args){
 System.out.println(greet(args[0]));
