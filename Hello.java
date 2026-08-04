29ce4d02c092cef802563fbfb75f5f81021a07ef
Kaveri0919 <kaverijakke9@gmail.com>
2026-08-04 11:03:42 +0530

    Change greeting message in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
 public static String greet(String name){
-return "HELLO" + " " +name;
+return "Welcome to devops lab" + " " +name;
 }
 public static void main(String[] args){
 System.out.println(greet(args[0]));
