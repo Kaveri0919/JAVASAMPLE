5606f637e266e58bcfec2b4bb5554964af67ce39
Kaveri0919 <kaverijakke9@gmail.com>
2026-08-11 11:38:16 +0530

    Fix if statement syntax in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,11 @@
 class Hello{
 public static void main(String[] args){ 
   String name=System.getenv("USERNAME_VAR");
-  if(name!=null);
+  if(name!=null){
   System.out.println("Welcome to devops lab" + "+name);
 }
   else{
 System.out.println("Environment variable not set!");
+}
 }
 }
