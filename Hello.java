1a73b8fc0c33f6e205bcf90cb5dccdbddaedf5ef
Kaveri0919 <kaverijakke9@gmail.com>
2026-08-11 11:41:29 +0530

    Fix string concatenation in welcome message

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ class Hello{
 public static void main(String[] args){ 
   String name=System.getenv("USERNAME_VAR");
   if(name!=null){
-    System.out.println("Welcome to devops lab" + "+name);
-}
+    System.out.println("Welcome to devops lab"+" "+name);
+  }
   else{
     System.out.println("Environment variable not set!");
   }
