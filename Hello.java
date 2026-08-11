7f06c1b9c70b9046650f9af419742ce936342b93
Kaveri0919 <kaverijakke9@gmail.com>
2026-08-11 11:39:57 +0530

    Fix output statements in Hello.java

--- Hello.java
+++ Hello.java
@@ -2,10 +2,10 @@ class Hello{
 public static void main(String[] args){ 
   String name=System.getenv("USERNAME_VAR");
   if(name!=null){
-  System.out.println("Welcome to devops lab" + "+name);
+    System.out.println("Welcome to devops lab" + "+name);
 }
   else{
-System.out.println("Environment variable not set!");
-}
+    System.out.println("Environment variable not set!");
+  }
 }
 }
